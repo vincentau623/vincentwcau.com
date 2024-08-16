85d748355dd40833f033a1e56c629d5fa0844da1
Vincent Au <vincent.au@housingauthority.gov.hk>
2024-08-16 14:23:42 +0800

    fix: attempt to fix cosmos connection issue

--- src/lib/cosmos.tsx
+++ src/lib/cosmos.tsx
@@ -9,13 +9,19 @@ class CosmosSingleton {
   }
 
   async initialize() {
+    console.log("start initialize cosmosdb");
     if (!this.database || !this.container) {
-      if (process.env.COSMOSDB_DATABASE_NAME === undefined || process.env.COSMOSDB_CONTAINER_NAME === undefined || process.env.COSMOSDB_CONNECTION_STRING === undefined) {
+      if (process.env.COSMOSDB_DATABASE_NAME === undefined ||
+        process.env.COSMOSDB_CONTAINER_NAME === undefined ||
+        process.env.COSMOSDB_CONNECTION_STRING === undefined ||
+        process.env.COSMOSDB_ENDPOINT === undefined ||
+        process.env.COSMOSDB_KEY === undefined) {
         throw new Error("COSMOSDB_DATABASE_NAME, COSMOSDB_CONTAINER_NAME, and COSMOSDB_CONNECTION_STRING must be set in the environment variables");
       }
       const databaseName = process.env.COSMOSDB_DATABASE_NAME;
       const containerName = process.env.COSMOSDB_CONTAINER_NAME;
-      const client = new CosmosClient(process.env.COSMOSDB_CONNECTION_STRING);
+      const client = new CosmosClient({ endpoint: process.env.COSMOSDB_ENDPOINT, key: process.env.COSMOSDB_KEY });
+      
       const database = client.database(databaseName);
       const container = database.container(containerName);
       await client.databases.createIfNotExists({
