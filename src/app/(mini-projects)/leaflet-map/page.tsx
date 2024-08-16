73672886d5d6bce33c10294d9df983e4a2e16696
Vincent Au <vincent.au@housingauthority.gov.hk>
2024-08-16 17:08:45 +0800

    feat: Add HKTrafficData API call and console.log

--- src/app/(mini-projects)/leaflet-map/page.tsx
+++ src/app/(mini-projects)/leaflet-map/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 
+import { getHKTrafficData } from "@/lib/map";
 import dynamic from "next/dynamic";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
 const LeafletMapPage = () => {
 
   const Map = useMemo(() => dynamic(() => import("@/components/Map"), { loading: () => <p>Map is loading</p>, ssr: false }), []);
 
+  useEffect(() => {
+    const init = async () => {
+      const data = await getHKTrafficData();
+      console.log(data);
+    };
+    init();
+  }, []);
+
   return (
     <div>
-      <Map/>
+      <Map />
     </div>
   );
 };
