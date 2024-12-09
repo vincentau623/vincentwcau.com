83da098c911db67e4cb63ba4baa5a3707dd9eb5a
Vincent Au <vincentwcau@gmail.com>
2024-12-09 12:15:50 -0500

    update UI pages

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,26 +1,52 @@
 import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
-import { Card, CardBody, CardFooter, CardHeader, Divider } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+} from "@nextui-org/react";
+import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
 
-const InfoCard = (props: { title: string, description: string; href: string; }) => {
+export const GithubIcon = ({ className }: { className?: string }) => {
+  return <Image className={className} src="/images/github-mark-white.svg" alt="github" width={50} height={50} />;
+};
+
+const InfoCard = (props: {
+  title: string;
+  description: string;
+  github?: string;
+  href: string;
+}) => {
   return (
     <Card className="m-4 w-3/4">
-      <CardHeader className="flex gap-3">
-        {props.title}
-      </CardHeader>
+      <CardHeader className="flex gap-3">{props.title}</CardHeader>
       <Divider />
-      <CardBody>
-        {props.description}
-      </CardBody>
+      <CardBody>{props.description}</CardBody>
       <Divider />
       <CardFooter>
-        <Link color="foreground" href={props.href} className="w-full text-right">
+        {props.github && (
+          <Link
+            color="foreground"
+            href={props.github}
+            className="w-full text-left"
+          >
+            <GithubIcon className="inline size-6" />
+          </Link>
+        )}
+        <Link
+          color="foreground"
+          href={props.href}
+          className="w-full text-right"
+        >
           <ArrowRightCircleIcon className="inline size-6" />
         </Link>
       </CardFooter>
-    </Card>)
-    ;
+    </Card>
+  );
 };
 
 export default InfoCard;
